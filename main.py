d85cf985caa21c0c33ae2a57525b4db5db5d5d10
browndataguy <138540026+browndataguy@users.noreply.github.com>
2023-07-05 01:48:57 +0000

    added HTML CSS and learnt a lot.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 main = Flask(__name__)
 
 @main.route("/")
 def hello_world():
-  return "Hello world"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   main.run(host = '0.0.0.0', debug = True)

b7c4a87dee45da72a96773e2f985be28c5fe5375
browndataguy <138540026+browndataguy@users.noreply.github.com>
2023-07-13 04:27:35 +0000

    added dynamic pages for each job

--- database.py
+++ database.py
@@ -3,18 +3,15 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
+engine = create_engine(db_connection_string,
+                       pool_pre_ping=True,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
-engine = create_engine(
-  db_connection_string,
-   pool_pre_ping=True,
-  connect_args={
-    "ssl": {
-        "ssl_ca": "/etc/ssl/cert.pem"
-    }
-    })
 
 def loads_jobs_from_db():
-# connecting the data with the engine
+  # connecting the data with the engine
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
@@ -24,8 +21,33 @@ def loads_jobs_from_db():
 
 
 
-  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    row = result.fetchone()
+    return row._asdict()
+    
+# def load_job_from_db(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(
+#       text(f"select * from jobs WHERE id = {id}") )
+
+
+#     rows = []
+#     for row in result.all():
+#       rows.append(row._mapping)
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return row
+      
+    # rows = result.all()
+    # if len(rows) == 0:
+    #   return None
+    # else:
+    #   return dict(rows[0])
+      
+
   # print("type result:", type(result))
   # print("type result.all:", type(result.all()))
   # print("result.all():", result_all)
-
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, jsonify
-from database import loads_jobs_from_db
+from database import loads_jobs_from_db, load_job_from_db
 
 main = Flask(__name__)
 
 @main.route("/")
 def hello_world():
   jobs = loads_jobs_from_db()
-  return render_template('home.html', jobs=jobs,
-                        company_name = 'Jovian')
+  return render_template('home.html', jobs=jobs)
 
 @main.route("/api/jobs")
 def list_jobs():
   jobs = loads_jobs_from_db()
   return jsonify(jobs)
+
+@main.route("/job/<id>")
+def show_jo(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Not found", 404
+    
+  return render_template('jobpage.html', job = job)
 
 if __name__ == "__main__":
   main.run(host = '0.0.0.0', debug = True)

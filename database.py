2060e51e071f19d7dd4a32b99e1c969c8b320dec
browndataguy <138540026+browndataguy@users.noreply.github.com>
2023-07-15 01:59:53 +0000

    Store applications in DB

--- database.py
+++ database.py
@@ -26,7 +26,23 @@ def load_job_from_db(id):
     result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
     row = result.fetchone()
     return row._asdict()
-    
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO application (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES(:job_id, :full_name,:email, :linkedin_url, :education,:work_experience, :resume_url) ")
+
+    conn.execute(query, 
+             {
+                 'job_id': job_id,
+                 'full_name': data['full_name'],
+                 'email': data['email'],
+                 'linkedin_url': data['linkedin_url'],
+                 'education': data['education'],
+                 'work_experience': data['work_experience'],
+                 'resume_url': data['resume_url']
+                 })
+      
 # def load_job_from_db(id):
 #   with engine.connect() as conn:
 #     result = conn.execute(
--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-from flask import Flask, render_template, jsonify
-from database import loads_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import loads_jobs_from_db, load_job_from_db, add_application_to_db
 
 main = Flask(__name__)
+
 
 @main.route("/")
 def hello_world():
   jobs = loads_jobs_from_db()
   return render_template('home.html', jobs=jobs)
 
+
 @main.route("/api/jobs")
 def list_jobs():
   jobs = loads_jobs_from_db()
   return jsonify(jobs)
 
+
 @main.route("/job/<id>")
 def show_jo(id):
   job = load_job_from_db(id)
-  
+
   if not job:
     return "Not found", 404
-    
-  return render_template('jobpage.html', job = job)
+
+  return render_template('jobpage.html', job=job)
+
+
+@main.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  #store this in the DB
+  # send an email
+  
+  return render_template('application_submitted.html',
+                        application=data,
+                        job = job)
+
 
 if __name__ == "__main__":
-  main.run(host = '0.0.0.0', debug = True)
- 
+  main.run(host='0.0.0.0', debug=True)
